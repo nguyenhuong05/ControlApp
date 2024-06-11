a2ecc8d0ec92f50a3c14ef98eff820064696020d
nguyenhuong05 <nguyenthuhuongmeomeo@email.com>
2024-06-11 13:01:50 +0700

    first commit

--- App.tsx
+++ App.tsx
@@ -1,117 +1,101 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useState} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
+  TextInput,
   Text,
-  useColorScheme,
   View,
+  TouchableOpacity,
 } from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const [setting, setControl] = useState('');
+  const [displayText, setDisplayText] = useState('');
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const handleSettingInput = (text: React.SetStateAction<string>) => {
+    setControl(text);
+  };
+  const handleSubmit = () => {
+    setDisplayText(`${setting}°C`);
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.textdisplay}>
+        Nhập ngưỡng nhiệt độ để bật tắt thiết bị
+      </Text>
+      <View style={styles.circle}>
+        <Text style={styles.circleText}>{displayText || '0°C'}</Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          onChangeText={handleSettingInput}
+          value={setting}
+          placeholder="Nhập ngưỡng nhiệt độ"
+          keyboardType="numeric"
+        />
+        <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+          <Text style={styles.buttonText}>Xác nhận</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: '#99D9F2',
+    paddingTop: 30,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  inputContainer: {
+    marginBottom: 20,
   },
-  sectionDescription: {
-    marginTop: 8,
+  input: {
+    height: 45,
+    width: 330,
+    margin: 12,
+    borderWidth: 1.5,
+    borderRadius: 20,
+    borderColor: 'white',
+    padding: 10,
+  },
+  textdisplay: {
     fontSize: 18,
-    fontWeight: '400',
   },
-  highlight: {
-    fontWeight: '700',
+  button: {
+    backgroundColor: '#007bff',
+    padding: 13,
+    borderRadius: 20,
+    width: 330,
+    height: 45,
+    marginLeft: 12,
+    gap: 10,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  circle: {
+    marginTop: 30,
+    backgroundColor: 'white',
+    borderRadius: 200,
+    width: 300,
+    height: 300,
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 4,
+    shadowOffset: {width: 0, height: 2},
+    shadowColor: 'black',
+    shadowOpacity: 0.25,
+    shadowRadius: 4.84,
+  },
+  circleText: {
+    fontSize: 30,
+    color: 'black',
   },
 });
 
